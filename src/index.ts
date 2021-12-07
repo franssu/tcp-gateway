af98b6950b3916223cf0e4cf22d521e4192d3666
franssu <openponey@gmail.com>
2021-12-07 12:05:39 +0100

    modif commitées bien après le dev

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,87 @@
 import net from 'net'
+import { program } from 'commander'
+
+function myParseInt(value: string, preivous: number): number {
+    // meh, si ca foire ?
+    return parseInt(value);
+}
+
+program
+    .requiredOption('--target-addr <target-addr>', 'addresse cible')
+    .requiredOption('--target-port <target-port>', 'port cible', myParseInt)
+    .requiredOption('--local-port <local-port>', 'port local', myParseInt)
+    .name('tcp-gateway')
+    .on('--help', () => {
+        console.log('')
+        console.log('Description: ')
+        console.log('  Ouvre un serveur local sur le port <local-port>, et redirige les paquet vers <target-addr>:<target-port>')
+    })
+
+program.parse(process.argv)
 
 const hostname = '0.0.0.0'
-const port = 10691;
 
-const connectionListener = (consoleSocket: net.Socket) => {
-    let isOnError = false
-    
-    const aquilonSocket = new net.Socket()
-    const connectToAquilon = () => aquilonSocket.connect(10691, "192.168.0.104")
-    connectToAquilon()
-    aquilonSocket.on('connect', () => {
-        console.log('connected to aquilon')
-        consoleSocket.pipe(aquilonSocket)
-        aquilonSocket.pipe(consoleSocket)
+const connectionListener = (clientSocket: net.Socket) => {
+    clientSocket.setKeepAlive(true, 500)
+    console.log('new client connected')
+
+    let isClientSocketClosed = false
+    clientSocket.on('close', () => { 
+        console.log('connection to client closed')
+        isClientSocketClosed = true
     })
-    aquilonSocket.on('error', (e) => {
-        // consoleSocket.pause() // checker si ca sert vraiment
-        if (!isOnError) {
-            isOnError = true
-            process.stdout.write('Retrying connection with aquilon...')
+    clientSocket.on('error', () => console.log('client socket error'))
+
+    const connectToTarget = () => {
+        let targetSocket = new net.Socket()
+
+        const cleanTargetSocket = () => {
+            targetSocket.removeAllListeners()
+            clientSocket.unpipe(targetSocket)
+            targetSocket.unpipe(clientSocket)
+        }
+
+        targetSocket.on('connect', () => {
+            console.log('connected to target')
+            clientSocket.pipe(targetSocket)
+            targetSocket.pipe(clientSocket)
+        })
+        targetSocket.on('error', (e) => console.log(`target socket error: \n${e}`))
+        targetSocket.on('close', () => {
+            console.log('connection to target closed')
+            cleanTargetSocket()
+            if (!isClientSocketClosed)
+                reconnect()
+        })
+
+        targetSocket.connect(program.targetPort, program.targetAddr)
+    }
+
+    let isReconnecting = false
+    const reconnect = () => {
+        if (!isReconnecting) {
+            isReconnecting = true
+            process.stdout.write('Retrying connection with target...')
         }
         setTimeout(() => {
             process.stdout.write('.')
-            connectToAquilon()
+            connectToTarget()
         }, 3000);
-    });
-    console.log('console connected')
+    }
+
+    connectToTarget()
 }
 
 const server = net.createServer(connectionListener)
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at ${hostname}:${port}/`);
+server.listen(program.localPort, hostname, () => {
+  console.log(`Server running at ${hostname}:${program.localPort}/`);
 });
+
+server.on('error', (err) => {
+    console.log(`local server error: ${err}`)
+})
+
+server.on('close', () => {
+    console.log('local sever closed')
+})
